56f688ef82ffdc859407999b97e62812a566e8b7
mmircea97 <135026462+mmircea97@users.noreply.github.com>
2025-11-16 01:12:03 +0200

    MapReveal for lvl 1

---
+++ ECHO_GameJam25/Assets/Scripts2/MapReveal.cs
@@ -0,0 +1,29 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MapReveal : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameObject;
+
+    [SerializeField]
+    private float maxDistance;
+
+    private float dimension;
+
+    [SerializeField]
+    private float scaleMultiplier;
+
+    void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        dimension = (1 / Vector2.Distance(this.transform.position, gameObject.transform.position) / maxDistance) * scaleMultiplier;
+        Debug.Log(Vector2.Distance(this.transform.position, gameObject.transform.position));
+        this.transform.localScale = new Vector3(dimension, dimension, dimension);
+    }
+}
