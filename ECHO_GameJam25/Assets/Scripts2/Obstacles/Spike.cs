87ce64b3a64de4491c7e99d18840445a86e3f7c1
mmircea97 <135026462+mmircea97@users.noreply.github.com>
2025-11-16 08:30:21 +0200

    FinalController

--- ECHO_GameJam25/Assets/Scripts2/Obstacles/Spike.cs
+++ ECHO_GameJam25/Assets/Scripts2/Obstacles/Spike.cs
@@ -1,11 +1,46 @@
-using System;
-using UnityEditor;
 using UnityEngine;
 
 public class Spike : MonoBehaviour
-{ 
+{
+    [Header("Respawn Points")]
+    public Transform[] respawnPoints;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        EditorApplication.ExitPlaymode();
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Respawn(collision.gameObject);
+        }
+    }
+
+    void Respawn(GameObject player)
+    {
+        if (respawnPoints.Length == 0)
+        {
+            Debug.LogWarning("Nu existã puncte de respawn!");
+            return;
+        }
+
+
+        Transform nearest = respawnPoints[0];
+        float minDistance = Vector2.Distance(player.transform.position, nearest.position);
+
+        for (int i = 1; i < respawnPoints.Length; i++)
+        {
+            float dist = Vector2.Distance(player.transform.position, respawnPoints[i].position);
+            if (dist < minDistance)
+            {
+                minDistance = dist;
+                nearest = respawnPoints[i];
+            }
+        }
+
+
+        player.transform.position = nearest.position;
+
+
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+        if (rb != null)
+            rb.linearVelocity = Vector2.zero;
     }
 }
