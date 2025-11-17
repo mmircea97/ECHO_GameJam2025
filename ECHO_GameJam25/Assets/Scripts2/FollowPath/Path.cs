3def5e643594e7ebbd83cbaf5086237f1fee5f7f
mmircea97 <135026462+mmircea97@users.noreply.github.com>
2025-11-17 19:48:32 +0200

    Light not fixed fully yet

--- ECHO_GameJam25/Assets/Scripts2/FollowPath/Path.cs
+++ ECHO_GameJam25/Assets/Scripts2/FollowPath/Path.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Path : MonoBehaviour
 {
@@ -6,34 +7,72 @@ public class Path : MonoBehaviour
     private Transform[] Points;
 
     [SerializeField]
+    private GameObject light;
+
+    [SerializeField]
     private float moveSpeed;
 
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private GameObject respawnPoint;
+
     private int pointIndex;
+
+    private Vector3 initialTriggerPos;
+    private Vector3 initialLightPos;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = Points[pointIndex].transform.position;
+        pointIndex = 0;
+        light.transform.position = Points[pointIndex].transform.position;
+        initialTriggerPos = transform.position;
+        initialLightPos = light.transform.position;
+
+
     }
 
     // Update is called once per frame
     void Update()
     { 
+
+        //Reseter
+        if(player.transform.position == respawnPoint.transform.position)
+        {
+            pointIndex = 0;
+            transform.position = initialTriggerPos;
+            light.transform.position = initialLightPos;
+        }
+
+
         if (pointIndex <= Points.Length - 1)
 
         {
-            transform.position = Vector3.MoveTowards(transform.position, Points[pointIndex].transform.position, moveSpeed * Time.deltaTime);
-
-            if (transform.position == Points[pointIndex].transform.position)
-            {
-                pointIndex += 1;
-            }
-            
-            if(pointIndex == Points.Length)
-            {
-                pointIndex = 0;
-            }
+            light.transform.position = Vector3.MoveTowards(light.transform.position, Points[pointIndex].transform.position, moveSpeed * Time.deltaTime);
         }
+
+        
             
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //moves the light
+        if(light.transform.position == Points[pointIndex].transform.position) 
+            pointIndex += 1;
+        //moves the trigger
+        if (pointIndex != 0)
+            transform.position = Points[pointIndex - 1].transform.position;
+        else
+        {
+            transform.position = initialLightPos;
+        }
+
+        if (pointIndex == Points.Length)
+        {
+            pointIndex = 0;
+        }
     }
 }
